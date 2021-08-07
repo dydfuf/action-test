ed909add4bf0e0424ae3f610556a90d3798a225c
dydfuf <88dydfuf@naver.com>
2021-08-08 01:09:37 +0900

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+def get_latest_post(rss_feed):
+    f = feedparser.parse(rss_feed)
+    latest_post = sorted(f['entries'], key=lambda x:x['published_parsed'])[-1]
+    return {
+            'title': latest_post['title'],
+            'link': latest_post['link'],
+            }
+
+template = Template(Path('./README_template.md').read_text())
+Path('./README.md').write_text(
+        template.render(
+            latest_post=get_latest_post(rss_feed),
+            latest_podcast_post=get_latest_post(podcast_url),
+            )
+        )
